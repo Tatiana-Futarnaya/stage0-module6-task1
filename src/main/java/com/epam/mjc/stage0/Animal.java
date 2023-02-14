80cc07c1b1dbbd72a10d9c8617d43a6518f6ee64
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-14 12:26:35 +0300

    first commit

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,32 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal() {
+    }
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String word1="paw";
+        String word2="no";
+
+        if(numberOfPaws>1){
+            word1="paws";
+        }
+
+        if (hasFur==true){
+            word2="a";
+        }
+        return "This animal is mostly "+ color +
+                ". It has "+numberOfPaws+" "+word1+" " +
+                "and "+word2+" fur.";
+    }
 }
